5d18e413bb78f378b2f5b46a0fe5f77f05625b8f
emrus <emrus0508@gmail.com>
2025-10-28 14:16:42 +0500

    hw12  done

---
+++ M3/5/commands.py
@@ -0,0 +1,155 @@
+from datetime import datetime
+
+class Clock():
+    def show_time(self):
+        print(datetime.now().strftime('%H:%M:%S'))
+
+class Calculator():
+    def add(self, a, b):
+        return a+b
+
+class Camera():
+    def take_shot(self):
+        print('Bling!')
+
+class SmartPhone(Camera, Clock, Calculator):
+    def ring(self):
+        print('Na-na-na-na, na-na-na-na,na-na-na-na!')
+
+nokia = SmartPhone()
+
+# nokia.show_time()
+# print(nokia.add(1000, 2000))
+# nokia.take_shot()
+# nokia.ring()
+
+#=============================================================
+class FirstParent():
+    def where(self):
+        print('In first parent')
+
+class SecondParent():
+    def where(self):
+        print('In second parent')
+
+class Child(FirstParent, SecondParent):
+    pass
+
+child = Child()
+# child.where()
+# =============================================================
+class Item():
+    def __init__(self):
+        self.public_value = 10
+        self._protected_value = 20
+
+    def public_method(self):
+        print('Public')
+
+    def _protected_method(self):
+        print('Protected')
+
+# item = Item()
+# print(item.public_value)
+# print(item._protected_value)
+# item.public_method()
+# item._protected_method()
+
+class Parent():
+    def __init__(self):
+        self.__private_value = 30
+
+    def __private_method(self):
+        print('Private')
+
+# item = Parent()
+
+# print(item.__private_value)
+# item.__private_method()
+#
+# print(item._Parent__private_value)
+# item._Parent__private_method()
+
+
+# class Child(Parent):
+#     def call_private_method(self):
+#         self.__private_method()
+#
+#     def get_private_value(self):
+#         return self.__private_value
+
+# item = Child()
+# print(item.get_private_value())
+# item.call_private_method()
+
+class Child(Parent):
+    def call_private_method(self):
+        self._Parent__private_method()
+
+    def get_private_value(self):
+        return self._Parent__private_value
+
+# item = Child()
+# print(item.get_private_value())
+# item.call_private_method()
+# =============================================================
+# class Person:
+#     def __init__(self, name, age):
+#         self._name = name
+#         self.__age = age
+#
+#     def get_age(self):
+#         return self.__age
+#
+#     def set_age(self, age):
+#         if 0 <= age <= 120:
+#             self.__age = age
+#         else:
+#             print("Недопустимый возраст")
+#
+#     def  get_name(self):
+#         return self._name
+#
+#     def set_name(self, name):
+#         self._name = name
+
+# person = Person("Alice", 30)
+# print(person.get_age())
+# person.set_age(100)
+# print(person.get_age())
+# person.get_age()
+# person.set_age(50)
+# =============================================================
+class Person:
+    def __init__(self, name, age):
+        self._name = name
+        self.__age = age
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        if 0 <= age <= 120:
+            self.__age = age
+
+        else:
+            print("Недопустимый возраст")
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+person = Person("Alice", 30)
+print(person.age)
+print(person.name)
+person.age = 35
+print(person.age)
+person.age = 150
+
+
---
+++ M3/5/hw12.py
@@ -0,0 +1,54 @@
+class BankAccount:
+    def __init__(self,__balance,__account_number,_owner):
+        self.__balance = __balance
+        self.__account_number = __account_number
+        self._owner = _owner
+
+    def deposit(self,amount):
+        self.__balance += amount
+
+    def withdraw(self,amount):
+        if self.__balance < amount:
+            print('Insufficient funds')
+        else:
+            self.__balance -= amount
+
+    def account_info(self):
+        return f'Balance : {self.__balance}  \nAccount Number : {self.__account_number}  \nOwner : {self._owner}'
+
+    @property
+    def owner(self):
+        return self._owner
+
+
+    @owner.setter
+    def owner(self,owner):
+        for char in owner:
+            if char.isdigit():
+                print('No numbers allowed')
+                break
+        else:
+            self._owner = owner
+
+
+
+    @property
+    def get_balance(self):
+        return self.__balance
+
+    @property
+    def get_account_number(self):
+        return self.__account_number
+
+client_1 = BankAccount(10000,123456,"Marcus")
+client_1.__balance = 50000
+print(client_1.get_balance)
+client_1.__account_number = 888888
+print(client_1.get_account_number)
+client_1.deposit(50000)
+print(client_1.get_balance)
+client_1.withdraw(30000)
+print(client_1.get_balance)
+client_1._owner = 'Anthony'
+print(client_1.owner)
+print(client_1.account_info())
