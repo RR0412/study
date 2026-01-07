1006225fad975e558f60f83abc9c55db58023088
emrus <emrus0508@gmail.com>
2026-01-07 15:22:34 +0500

    added url/view/html

--- M6/4/homework/projects/config/urls.py
+++ M6/4/homework/projects/config/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',todo_views.index.view)
+    path('',todo_views.index_view)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
