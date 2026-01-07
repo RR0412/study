19547968fe4c4d4349dcb95ada28df48db1f476f
emrus <emrus0508@gmail.com>
2026-01-07 15:17:54 +0500

    added import settings

--- M6/4/homework/projects/config/urls.py
+++ M6/4/homework/projects/config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from todo import views as todo_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',todo_views.index.view)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
