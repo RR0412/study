869f6bd8dddd9af3c26aec5df757762aa1ec220b
emrus <emrus0508@gmail.com>
2026-01-11 11:32:32 +0500

    added new field details / made migrations / added note to new field

---
+++ M6/5/homework/projects/config/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from todo import views as todo_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('',todo_views.index_view),
+    path('task/',todo_views.task_page),
+    path('success/',todo_views.success_view),
+    path('task_list/',todo_views.task_list)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ M6/5/homework/projects/todo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Task(models.Model):
+    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name = 'Описание')
+    details = models.TextField(max_length=3000, null=True, blank=True, verbose_name = 'Детали')
+    status = models.CharField(max_length=200, null=False, blank=False, default= 'New', verbose_name='Статус')
+    due_date = models.DateField(null=True, blank=True,verbose_name='Дата выполнения')
+
+    def __str__(self):
+        return f'{self.description} - {self.status}'
