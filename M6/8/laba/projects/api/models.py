ebfb80afbebd20ae2ba63817833d12d12286e8b9
emrus <emrus0508@gmail.com>
2026-01-16 14:00:45 +0500

    added str method

--- M6/8/laba/projects/api/models.py
+++ M6/8/laba/projects/api/models.py
@@ -12,3 +12,7 @@ class Product(models.Model):
     description = models.TextField(max_length = 2000, null=True, blank=True)
     category = models.CharField(max_length = 20,null=False, blank=False, choices = CATEGORY_CHOICES, default = 'other')
     price = models.DecimalField(max_digits=7,decimal_places=2)
+
+    def __str__(self):
+        return f'{self.name}-{self.category}'
+    
