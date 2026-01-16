0a023a29eb8f7d96e975347fb4fce05d0d5e0a12
emrus <emrus0508@gmail.com>
2026-01-16 15:51:21 +0500

    added logic for product list( to display name/price/category

--- M6/8/laba/projects/api/views.py
+++ M6/8/laba/projects/api/views.py
@@ -3,14 +3,22 @@ from django.core import serializers
 from django.http import HttpResponse,HttpResponseNotAllowed
 from django.views.decorators.csrf import ensure_csrf_cookie
 from api.models import Product
+import json
 
 
 
 def products_view(request,*args,**kwargs):
     if request.method == 'GET':
         products = Product.objects.all()
-        products_data = serializers.serialize('json', products)
-        response = HttpResponse(products_data)
+        answer_list = []
+        for product in products:
+            answer = {}
+            answer['name'] = product.name
+            answer['price'] = str(product.price)
+            answer['category'] = product.category
+            answer_list.append(answer)
+        answer_as_json = json.dumps(answer_list)
+        response = HttpResponse(answer_as_json)
         response ['Content-Type'] = 'application/json'
         return response
     
