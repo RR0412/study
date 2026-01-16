07d45b111d9eebb7197d8286ca5693e70f4abc84
emrus <emrus0508@gmail.com>
2026-01-16 14:34:02 +0500

    |view for product list|view for csrf|loaded fixtures

--- M6/8/laba/projects/api/urls.py
+++ M6/8/laba/projects/api/urls.py
@@ -1,7 +1,12 @@
-# from django.urls import path
-# from api import views as api_views
+from django.urls import path
+from api import views as api_views
 
-# urlpatterns = [
-#     path('task_list/',api_views.task_list_view,name = 'task_list'),
-#     path('task/<int:pk>/',api_views.task_view, name = 'task')
-# ]
+urlpatterns = [
+    path('products/',api_views.products_view,name = 'products'),
+    path('token/',api_views.get_token_view,name = 'token'),
+    # path('task/<int:pk>/',api_views.task_view, name = 'task')
+]
+
+# X-CSRFToken
+
+# fWqrmpouGlSuaoZ6peCkcDUMJImSBW1j
--- M6/8/laba/projects/api/views.py
+++ M6/8/laba/projects/api/views.py
@@ -1,2 +1,21 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse,HttpResponseNotAllowed
+from django.views.decorators.csrf import ensure_csrf_cookie
+from api.models import Product
 
+
+
+def products_view(request,*args,**kwargs):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        products_data = serializers.serialize('json', products)
+        response = HttpResponse(products_data)
+        response ['Content-Type'] = 'application/json'
+        return response
+    
+@ensure_csrf_cookie
+def get_token_view(request,*args,**kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET requests are allowed')
