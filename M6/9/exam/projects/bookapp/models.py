6c646d339db3c75bc865fa41a8152dfab0cc2104
emrus <emrus0508@gmail.com>
2026-01-18 15:30:05 +0500

    added model

--- M6/9/exam/projects/bookapp/models.py
+++ M6/9/exam/projects/bookapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+STATUS_CHOICES = [
+    ('active','Активно'),
+    ('blocked','Заблокировано'),
+]
+class Book(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name = 'Имя')
+    email = models.EmailField(max_length=100, null=False, blank=False, verbose_name = 'Электронная почта')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name = 'Текст записи')
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_changed_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length = 20, null=False, blank=False, choices = STATUS_CHOICES, default = 'active')
 
-# Create your models here.
+    def __str__(self):
+        return f'{self.name} - {self.status}'
