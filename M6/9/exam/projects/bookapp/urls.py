3ccb2ec59947ff92b6e4e5170ea1e7907f50138e
emrus <emrus0508@gmail.com>
2026-01-19 13:53:55 +0500

    add form /view

--- M6/9/exam/projects/bookapp/urls.py
+++ M6/9/exam/projects/bookapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from bookapp import views as bookapp_views
 
-# urlpatterns = [
-#     path('/',bookapp_views.book_view,name = 'book'),
-#     path('book/')
-# ]
+urlpatterns = [
+    path('',bookapp_views.books_list,name = 'books'),
+    path('add/',bookapp_views.add_book, name = 'book_add'),
+]
--- M6/9/exam/projects/bookapp/views.py
+++ M6/9/exam/projects/bookapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from bookapp.models import Book
 
-# Create your views here.
+def books_list(request):   
+    sorted_books = Book.objects.all().order_by('-created_at')
+    books = sorted_books.filter(status='active')
+    return render(request, 'bookapp/books_list.html', {'books' : books})
+
+def add_book(request):
+    if request.method == 'GET':
+        return render(request, 'bookapp/book_add.html')
+    elif request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        text = request.POST.get('text')
+        book=Book.objects.create(name=name,email=email,text=text)
+        return redirect('books')
+
