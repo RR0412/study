b317622837c1d06bf1584e58839a25abe5b6ec69
emrus <emrus0508@gmail.com>
2026-01-19 17:49:32 +0500

    added delete

--- M6/9/exam/projects/bookapp/urls.py
+++ M6/9/exam/projects/bookapp/urls.py
@@ -4,4 +4,6 @@ from bookapp import views as bookapp_views
 urlpatterns = [
     path('',bookapp_views.books_list,name = 'books'),
     path('add/',bookapp_views.add_book, name = 'book_add'),
+    path('change/<int:pk>/',bookapp_views.change_book, name = 'book_change'),
+    path('delete/<int:pk>/',bookapp_views.delete_book, name = 'book_delete')
 ]
--- M6/9/exam/projects/bookapp/views.py
+++ M6/9/exam/projects/bookapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from bookapp.models import Book
 
 def books_list(request):   
@@ -13,6 +13,24 @@ def add_book(request):
         name = request.POST.get('name')
         email = request.POST.get('email')
         text = request.POST.get('text')
-        book=Book.objects.create(name=name,email=email,text=text)
+        Book.objects.create(name=name,email=email,text=text)
         return redirect('books')
 
+
+def change_book(request,pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        text = request.POST.get('text')
+        book = Book.objects.create(name=name,email=email,text=text)
+        book.save()
+        return redirect('books')
+    return render(request, 'bookapp/book_change.html',{'book': book})
+
+def delete_book(request,pk):
+    book=get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('books')
+    return render (request,'bookapp/book_confirm_delete.html',{'book': book})
