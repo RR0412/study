2e4fcef00f19cfb01207e8791b86f54cc7e5ac33
emrus <emrus0508@gmail.com>
2026-02-03 12:29:04 +0500

    task serializer

---
+++ M7/51/homework/projects/tracker/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from tracker import views
+
+urlpatterns = [
+    path('tasks/', views.TaskListCreateView.as_view()),
+    path('tasks/<int:pk>/', views.TaskReadUpdateDeleteView.as_view()),
+]
---
+++ M7/51/homework/projects/tracker/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from tracker.models import Task
+from tracker.serializers import TaskSerializer
+
+class TaskListCreateView(APIView):
+    def get(self,request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            task = Task.objects.create(
+                title=serializer.validated_data['title'],
+                description=serializer.validated_data['description'],
+                status=serializer.validated_data['status'],
+                type=serializer.validated_data['type']
+            )
+            return Response(TaskSerializer(task).data,
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskReadUpdateDeleteView(APIView):
+    def dispatch(self,request,*args,**kwargs):
+        self.task=get_object_or_404(Task, id=kwargs.get('pk'))
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_task_response(self):
+        serializer = TaskSerializer(instance=self.task)
+        return Response(serializer.data)
+    
+    def get(self, request, *args, **kwargs):
+        return self.get_task_response()
+    
+    def patch(self, request, *args, **kwargs):
+        for key,value in request.data.items():
+            setattr(self.task, key, value)
+
+        self.task.save()
+        return self.get_task_response()
+    
+    def delete(self, request, *args, **kwargs):
+        self.task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
